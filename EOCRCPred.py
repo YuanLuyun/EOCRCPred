f196408a173d0357fe45c2cf316fdd6798143480
YuanLuyun <462824817@qq.com>
2024-09-07 14:29:42 +0800

    Update EOCRCPred.py

--- EOCRCPred.py
+++ EOCRCPred.py
@@ -48,20 +48,21 @@ with col1:
     sex = st.selectbox("Sex", options=["Male", "Female"], index=0)
     race = st.selectbox("Race", options=["White", "Black", "Other"], index=0)
     grade = st.selectbox("Grade", options=[1.0, 2.0, 3.0, 4.0], index=0)
-    t = st.selectbox("T", options=[0.0, 1.0, 2.0, 3.0, 4.0], index=0)
+    
     resected_lns = st.selectbox("No. of Resected LNs", options=[0.0, 1.0, 2.0], index=0)
     income = st.selectbox("Median Household Income", options=[1.0, 2.0, 3.0, 4.0], index=0)
     
 
 with col2:
-    
+    tnm_stage = st.selectbox("TNM Stage", options=[0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0], index=0)
+    t = st.selectbox("T", options=[0.0, 1.0, 2.0, 3.0, 4.0], index=0)
     n = st.selectbox("N", options=[0.0, 1.0, 2.0], index=0)
     tumor_deposits = st.selectbox("Tumor Deposits", options=[0.0, 1.0, 2.0], index=0)
     
     marital_status = st.selectbox("Marital status", options=["Single", "Married", "Divorced", "Widowed"], index=0)
 
 with col3:
-    tnm_stage = st.selectbox("TNM Stage", options=[0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0], index=0)
+    
     cea = st.selectbox("CEA", options=[0.0, 1.0, 2.0], index=0)
     tumor_size = st.number_input("Tumor Size", min_value=0.0, max_value=10.0, step=0.1, value=0.0)
     histology = st.selectbox("Histology", options=["Non-specific adenocarcinoma", "Specific adenocarcinoma", "Other"], index=0)
