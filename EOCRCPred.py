8c1f521ff8db456342ddeb4cc80bc10f7fc67fd8
YuanLuyun <462824817@qq.com>
2024-09-08 12:08:08 +0800

    Update EOCRCPred.py

--- EOCRCPred.py
+++ EOCRCPred.py
@@ -205,7 +205,7 @@ if st.button("Submit"):
 
 
     # 输出累积风险曲线
-    fig, ax = plt.subplots(figsize=(5, 5))
+    fig, ax = plt.subplots()
     ax.plot(time_index, risks_matrix[0], label='Cumulative Hazard')
     ax.set_xlabel("Time (Months)")
     ax.set_ylabel("Cumulative Hazard")
